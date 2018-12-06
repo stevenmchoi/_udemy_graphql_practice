ac4e95a7199bda03b3e500c65936e32c93a04805
Steven Choi <steven.m.choi@gmail.com>
2018-12-05 22:49:54 -0800

    Have RootQuery use axios to call on db

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -1,11 +1,6 @@
 const graphql = require('graphql');
-const _ = require('lodash');
+const axios = require('axios');
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
-
-// const users = [
-// 	{ id: '23', firstName: 'Bill', age: 20 },
-// 	{ id: '47', firstName: 'Samantha', age: 21 },
-// ];
 
 const UserType = new GraphQLObjectType({
 	name: 'User',
@@ -23,7 +18,9 @@ const RootQuery = new GraphQLObjectType({
 			type: UserType,
 			args: { id: { type: GraphQLString } },
 			resolve(parentValue, args) {
-				return _.find(users, { id: args.id });
+				return axios
+					.get(`http://localhost:3000/users/${args.id}`)
+					.then((resp) => resp.data);
 			},
 		},
 	},
