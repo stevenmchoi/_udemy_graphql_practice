235348f758a56d9b67c197ea3b289b870253816d
Steven Choi <steven.m.choi@gmail.com>
2018-12-05 23:32:34 -0800

    Make relationship between company and user

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -2,12 +2,29 @@ const graphql = require('graphql');
 const axios = require('axios');
 const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
 
+const CompanyType = new GraphQLObjectType({
+	name: 'Company',
+	fields: {
+		id: { type: GraphQLString },
+		name: { type: GraphQLString },
+		description: { type: GraphQLString },
+	},
+});
+
 const UserType = new GraphQLObjectType({
 	name: 'User',
 	fields: {
 		id: { type: GraphQLString },
 		firstName: { type: GraphQLString },
 		age: { type: GraphQLInt },
+		company: {
+			type: CompanyType,
+			resolve(parentValue, args) {
+				return axios
+					.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+					.then((res) => res.data);
+			},
+		},
 	},
 });
 
@@ -20,7 +37,7 @@ const RootQuery = new GraphQLObjectType({
 			resolve(parentValue, args) {
 				return axios
 					.get(`http://localhost:3000/users/${args.id}`)
-					.then((resp) => resp.data);
+					.then((res) => res.data);
 			},
 		},
 	},
